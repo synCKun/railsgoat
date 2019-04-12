bc057737055b67f453938a497403b86c0fddac58
Stefan Streichsbier <stefan@streichsbier.at>
2019-04-13 00:31:05 +0800

    fix secret issue

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_live_3cC2OUaORT6Sgitt1ltPeEA1',
-  :secret_key      => 'sk_live_OaASXG1s0xABCp0k5d8AsbaL'
-}
+    Rails.configuration.stripe = {
+        :publishable_key => ENV['PUBLISHABLE_KEY'],
+        :secret_key      => ENV['SECRET_KEY']
+    }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+    Stripe.api_key = Rails.configuration.stripe[:secret_key]
