0cdb04f17040f8f8a17e840f7190616cb48931cb
CK <47073395+synCKun@users.noreply.github.com>
2019-04-13 10:38:34 +0800

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 rails.configuration.striple = {
   :publishable_key => 'pk_live_3cC20Ua0RT6Sgitt1ltPeEA1',
-  :secret_key => 'sk_liver_0aASXG1xABCp0k5d8AsbaL'
+  :secret_key => 'sk_liver_0aASXG1xABCp0k5d8AsbaLaa'
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
